bdd2b15969719fbca01a9ac14b24d6792a4c7742
Darren Douglas Lee aka Dee <dlee@ubersmith.com>
2017-03-22 00:32:47 -0400

    ISSUE-7 : Add supporting comments to explain some of the test process

--- tests/LostSoulTest.php
+++ tests/LostSoulTest.php
@@ -23,9 +23,17 @@ final class LostSoulTest extends TestCase
   public function testReciprocatesHugs()
   {
     $lostSoul = new LostSoul();
+
+    // PHPUnit 4.5 and Prophecy
+    // https://thephp.cc/news/2015/02/phpunit-4-5-and-prophecy
     $mock = $this->prophesize(Huggable::class);
+
+    // Confirm the two LostSoul objects exchange hugs
     $mock->hug($lostSoul)->shouldBeCalled();
+
+    // reveal the prophecy and create an actual test double object
     $lostSoul->hug($mock->reveal());
+
   }
 
 }
