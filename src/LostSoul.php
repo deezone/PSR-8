100c5f15cacbc044b7db345a385abad5cd9b7002
Darren Douglas Lee aka Dee <dlee@ubersmith.com>
2017-03-25 17:31:50 -0400

    ISSUE-8 : Added Exception for the case where a self hug is attempted

--- src/LostSoul.php
+++ src/LostSoul.php
@@ -36,6 +36,13 @@ class LostSoul implements Huggable
      */
     public function hug(Huggable $soul)
     {
+        // An Exception is thrown as self hugging suggests an error in implimentation. Not a show stopper but should
+        // be addressed.
+        if ($soul === $this) {
+          throw new \Exception('WARNING: You should always love yourself but self hugging is not supported in the PSR-8' .
+              ' specification. An attept at an object hugging itself has been made.');
+        }
+
         $hugBacksRequested = $this->minHugsRequired;
         while ($hugBacksRequested--) {
             $soul->hug($this);
